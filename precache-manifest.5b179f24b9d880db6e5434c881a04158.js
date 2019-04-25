c737ad713c36d945c76440e80013a1143e106cfd
John Desrosiers <john.desro@gmail.com>
2019-04-25 07:36:03 -0700

    Updates

---
+++ precache-manifest.5b179f24b9d880db6e5434c881a04158.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31b2299ae648b09b15861b9de864af64",
+    "url": "/react-memory-game/index.html"
+  },
+  {
+    "revision": "1b472991280af0649de1",
+    "url": "/react-memory-game/static/css/main.a2b7b0b6.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/react-memory-game/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "1b472991280af0649de1",
+    "url": "/react-memory-game/static/js/main.9da2a3c0.chunk.js"
+  },
+  {
+    "revision": "e8b71f681a914c951703",
+    "url": "/react-memory-game/static/js/runtime~main.83a7a993.js"
+  }
+]);
