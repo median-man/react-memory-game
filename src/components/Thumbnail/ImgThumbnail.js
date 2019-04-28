d05bb484ba85b8f8b72644b3320bcbea0d25b6ee
John Desrosiers <john.desro@gmail.com>
2019-04-28 09:43:34 -0700

    ImgThumbnail clears focus on click

--- src/components/Thumbnail/ImgThumbnail.js
+++ src/components/Thumbnail/ImgThumbnail.js
@@ -2,14 +2,20 @@ import React from 'react';
 
 export default (props) => {
   const { label, alt, fileName } = props;
+  const button = React.createRef()
+  const onClick = () => {
+    button.current.blur()
+    props.onClick()
+  }
   return (
     <span
-      onClick={props.onClick}
+      onClick={onClick}
       onKeyPress={props.onClick}
       role="button"
       tabIndex={0}
       aria-label={label}
       className="p-1 d-inline-block"
+      ref={button}
     >
       <img alt={alt} src={`images/${fileName}`} className="img-thumbnail" />
     </span>
